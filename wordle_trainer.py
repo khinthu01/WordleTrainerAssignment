8fbcee77f67ab676fcf7e0c1e585d0e6d21a6c9e
khinthu01 <khinthu01@gmail.com>
2022-03-24 13:55:43 +1100

    completed doc in wordle trainer

--- wordle_trainer.py
+++ wordle_trainer.py
@@ -5,15 +5,15 @@
 
 def counting_sort(arr, position):
     '''
-    This function sorts an array of strings in a non-decreasing order based on the letter at position using a non-comparison sorting algorithm.
+    This function sorts an array of tuples of strings in a non-decreasing order based on the letter at position in the second element in each tuple (key) using a non-comparison sorting algorithm.
 
-    :param arr: an array of strings that are all the same length and all characters are letters of the English alphabet
+    :param arr: an array of tuples with a word and its key (string, string). The word is located at index 0 in each tuple and the key which is all the word's letters in alphabetical order is located at index 1 in each tuple. All strings in the array are the same fixed length and contain only letters from the English alphabet
     :param position: position of the letter in the string that the array is supposed to sort on. For example, if position is 5 then the function will sort based on the sixth letter in all string elements in arr.
 
     :returns: a sorted array
 
     :time complexity: counting_sort has a time complexity of O(n*k) where n is the length of arr and k is the number of different letters in the alphabet. Since k is constant at 26 this results in an overall worst-case time complexity of O(n).
-    :space complexity: counting_sort has a space complexity of O(n) where n is the length of the sorted array which equals to the length of arr. 
+    :space complexity: counting_sort has a space complexity of O(n*m) where n is the length of the sorted array which equals to the length of arr and m is the length of each word in each tuple in the sorted array. 
     '''
 
     # creating a counting array of length 26 for each of the 26 characters using the ord() function to get the Unicode codes
@@ -49,9 +49,9 @@ def counting_sort(arr, position):
 
 def radix_sort(arr):
     '''
-    This function sorts an array of strings in alphabetical order using a non-comparison sorting algorithm called radix sort. 
+    This function sorts an array of tuples of strings in alphabetical order using a non-comparison sorting algorithm called radix sort. 
 
-    :param arr: an array of strings that are all the same length and all characters are letters of the English alphabet
+    :param arr: an array of tuples with a word and its key (string, string). The word is located at index 0 in each tuple and the key which is all the word's letters in alphabetical order is located at index 1 in each tuple. All strings in the array are the same fixed length and contain only letters from the English alphabet
 
     :returns: a sorted array
 
@@ -77,9 +77,10 @@ def right_binary_search(arr, target):
 
     :returns: returns the index of the rightmost target
 
-    :time complexity: counting_sort has a time complexity of O(n*k) where n is the length of arr and k is the number of different letters in the alphabet. Since k is constant at 26 this results in an overall worst-case time complexity of O(n).
-    :space complexity: counting_sort has a space complexity of O(n) where n is the length of the sorted array which equals to the length of arr. 
+    :time complexity: worst-case time complexity is O(logn) where n is the length of the array, arr
+    :space complexity: the search is done in place so the space complexity is O(1)
     '''
+
     lo = 0
     hi = len(arr)
 
@@ -96,6 +97,18 @@ def right_binary_search(arr, target):
     
 
 def left_binary_search(arr, target):
+    '''
+    This function searches for the leftmost target in an array of tuples which contains a word and its key (all the letters in the word arranged in alphabetical order). The search is done based on the key which is at index 1 in each tuple.
+
+    :param arr: an array of tuples with a word and its key (string, string). The word is located at index 0 in each tuple and the key which is all the word's letters in alphabetical order is located at index 1 in each tuple
+    :param target: a string that represents the key being searched for. The target MUST be in the input array, arr. 
+
+    :returns: returns the index of the leftmost target
+
+    :time complexity: worst-case time complexity is O(logn) where n is the length of the array, arr
+    :space complexity: the search is done in place so the space complexity is O(1)
+    '''
+
     lo = 0
     hi = len(arr)
 
@@ -116,13 +129,12 @@ def sort_word(word):
     '''
     This function sorts letters in the input word in alphabetical order. 
 
-    :param arr: an array of strings that are all the same length and all characters are letters of the English alphabet
-    :param position: position of the letter in the string that the array is supposed to sort on. For example, if position is 5 then the function will sort based on the sixth letter in all string elements in arr.
+    :param word: a string where all letters are lower case letters of the English alphabet
 
-    :returns: a sorted array
+    :returns: a string where all the letters in the input word have been sorted in alphabetical order
 
-    :time complexity: counting_sort has a time complexity of O(n*k) where n is the length of arr and k is the number of different letters in the alphabet. Since k is constant at 26 this results in an overall worst-case time complexity of O(n).
-    :space complexity: counting_sort has a space complexity of O(n) where n is the length of the sorted array which equals to the length of arr. 
+    :time complexity: counting_sort has a time complexity of O(m) where m is the length of the word
+    :space complexity: counting_sort has a space complexity of O(m) where m is the length of the input word 
     '''
 
     # creating a counting array of length 26 for each of the 26 characters using the ord() function to get the Unicode codes
@@ -153,9 +165,20 @@ def sort_word(word):
     return sorted_word
 
 def trainer(wordlist, word, marker):
+    '''
+    This function is a limited Wordle Trainer that takes a word and determines which string/s from wordlist is/are likely to satisfy the constraints set by marker. 
 
+    :param wordlist: a list of strings that are all the same fixed length. The strings are all in lower case and contain only letters of the English alphabet
+    :param word: a word from wordlist that represents a guess on Wordle.
+    :param marker: a list of the same length as word which shows which letters in word are in the correct place. The marker contains only 0's and 1's with 0 indicating the letter is in the target word but in the wrong place and 1 indication that the letter is in the correct place. 
 
-    # if marker shows that all 1's then the word is already correct - best case complexity is O(1)
+    :returns: a list of strings that are from wordlist which are anagrams of the input word and satisfies the contraints set by the input marker
+
+    :time complexity: O(nm) where n is the length of wordlist and m is the length of the input word
+    :space complexity: O(nm) where n is the length of wordlist and m is the length of the input word
+    '''
+
+    # if marker shows all 1's then the word is already correct - best case complexity is O(1)
     if marker == [1]*len(marker):
         return [word]
 
@@ -163,7 +186,7 @@ def trainer(wordlist, word, marker):
     tuple_list = []
 
     # a loop to created the tuple list O(nm) since the counting sort can be used to sort the word at O(m) complexity where m is the number of letters in elem
-    # space complexity is O(n)
+    # space complexity is O(n*m)
     for elem in wordlist:
         elem.lower()
         key = "".join(sort_word(elem)) 
@@ -177,6 +200,8 @@ def trainer(wordlist, word, marker):
     left_anagram = left_binary_search(sorted_list, anagram)
     right_anagram = right_binary_search(sorted_list, anagram)
 
+    # anagrams contains only the words in wordlist that are anagrams of the input word
+    # worst-case time complexity is O(n) if all words in wordlist are anagrams of the input word
     anagrams = sorted_list[left_anagram:right_anagram+1]
     anagram_words = []
 
@@ -187,7 +212,7 @@ def trainer(wordlist, word, marker):
     valid_anagrams = []
 
     #looping through marker and anagram_words to find words that fit the criteria of having different letters in spots where marker[i] == 0 and having the same letter as input word in spots where marker[i] == 1
-    # worst case time complexity = O(nm) where anagram_words is the same length as wordlist and n is the length of wordlist and m is the length of each word in wordlist
+    # worst case time complexity = O(nm) where anagram_words is the same length as wordlist and n is the length of wordlist and m is the length of marker
     for i in range(len(marker)):
         for j in range(len(anagram_words)):
             if marker[i] == 0 and anagram_words[j][i] != word[i]:
